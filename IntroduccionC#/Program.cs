773545f8befaf95c867c844e02cba1f727479bf3
Santiago Rojas <santirojastr12@gmail.com>
2023-12-23 15:59:29 -0500

    Creación de objetos

--- IntroduccionC#/Program.cs
+++ IntroduccionC#/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var sale = new Sale(15);
+//  Sale sale = new Sale();
+//  Sale sale = new();
+var message = sale.GetInfo();
+Console.WriteLine(message);
+
+class Sale
+{
+    public decimal Total { get; set; }
+    private decimal _some;
+
+    public Sale(decimal total)
+    {
+        Total = total;
+        _some = 8;
+    }
+
+    public string GetInfo()
+    {
+        return $"El total es {Total}";
+    }
+}
