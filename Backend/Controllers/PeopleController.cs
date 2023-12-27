60eb8057eca4bc75254f232066ae88fa6b6857c3
Santiago Rojas <santirojastr12@gmail.com>
2023-12-26 20:24:40 -0500

    Respuesta de Tipo de dato especifico

---
+++ Backend/Controllers/PeopleController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet("all")]
+        public List<People> GetPeople() => Repository.People;
+    }
+
+    public class Repository
+    {
+        public static List<People> People = new List<People>
+        {
+            new People() { Id = 1, Name = "Santiago", BirthDate = new DateTime(2005, 10, 14) },
+            new People() { Id = 2, Name = "Pedro", BirthDate = new DateTime(1992, 11, 3) },
+            new People() { Id = 3, Name = "Anna", BirthDate = new DateTime(1985, 1, 8) }
+        };
+    }
+
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c74d2f670376110be0ea599aab9fb7078681ab35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+359132d218a09030f4fc47b6cbf799ece19c3cd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
