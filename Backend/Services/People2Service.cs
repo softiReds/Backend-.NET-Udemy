d4d859fc3ab8a42101c2fa9d43555e0f5597baa2
Santiago Rojas <santirojastr12@gmail.com>
2023-12-26 21:14:50 -0500

    Inyección de dependencias

---
+++ Backend/Services/People2Service.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Controllers;
+
+namespace Backend.Services
+{
+    public class People2Service : IPeopleService
+    {
+        public bool Validate(People people)
+        {
+            if (string.IsNullOrEmpty(people.Name) || people.Name.Length > 100)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
