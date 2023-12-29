4d2606390dd5a52bc51be8a5070b2261d776ef7a
Santiago Rojas <santirojastr12@gmail.com>
2023-12-28 21:39:07 -0500

    Task y sincrono vs asincrono

--- Backend/Controllers/SomeController.cs
+++ Backend/Controllers/SomeController.cs
@@ -26,5 +26,44 @@ namespace Backend.Controllers
 
             return Ok(stopwatch.Elapsed);   // Elapsed -> Obtiene el tiempo total del cronometro
         }
+
+        [HttpGet("async")]
+        public async Task<IActionResult> GetAsync()   //  async -> Especifica que el metodo será asíncrona; Task<Class> -> Representa una operación asincronica que puede incluir un resultado
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            var task1 = new Task<int>(() => //  Task puede o no retornar algo. SI no retorna nada, no se ponen los signos <>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Conexión a base de datos terminada");
+
+                return 10;
+            });
+
+            var task2 = new Task<int>(() => 
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Conexión a base de datos terminada");
+
+                return 20;
+            });
+
+            task1.Start();
+            task2.Start();
+
+            Console.WriteLine("Hago otra cosa");
+
+            //  await task1;    //  await -> No continua la ejecución hasta que termine la ejecución del Task (en caso de no haber terminado antes)
+
+            var resultTask1 = await task1;
+            var resultTask2 = await task2;
+
+            await Console.Out.WriteLineAsync("Todo ha terminado");
+
+            stopwatch.Stop();
+
+            return Ok($"{resultTask1} {resultTask2} {stopwatch.Elapsed}");
+        }
     }
 }
