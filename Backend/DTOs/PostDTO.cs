9524e0098d46ab3d035b41d921aa285cd024e10b
Santiago Rojas <santirojastr12@gmail.com>
2024-01-01 17:09:16 -0500

    Creación de DTO

---
+++ Backend/DTOs/PostDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Backend.DTOs
+{
+    public class PostDTO
+    {
+        /*
+         * Campos:
+         *  int Id
+         *  int UserId
+         *  string Title
+         *  string Body
+        */
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string? Title {  get; set; }
+        public string? Body { get; set; }
+    }
+}
