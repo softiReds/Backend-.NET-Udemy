eb6434804d2095fa9e785972231d287db3003070
Santiago Rojas <santirojastr12@gmail.com>
2024-01-02 19:26:07 -0500

    Creación de DTOs para CRUD

---
+++ Backend/DTOs/BeerUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.DTOs
+{
+    public class BeerUpdateDto
+    {
+        public string Name { get; set; }
+        public int BrandID { get; set; }
+        public decimal Alcohol { get; set; }
+    }
+}
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2da73d26c8fa79645121d77d66b8c5a45b5377b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
