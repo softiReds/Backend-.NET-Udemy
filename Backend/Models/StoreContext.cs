6b030aa87653ab36b2834198649463ef0f59413c
Santiago Rojas <santirojastr12@gmail.com>
2024-01-02 19:12:23 -0500

    Migración inicial

--- Backend/Models/StoreContext.cs
+++ Backend/Models/StoreContext.cs
@@ -4,6 +4,7 @@ namespace Backend.Models
 {
     public class StoreContext : DbContext
     {
+        //  Una forma de inicializar las migraciones es: Abrir la consola de paquetes NuGet, Add-Migration migrationName
         public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
 
         public DbSet<Beer> Beers { get; set; }
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47b74bbc530088f7c9fa2640a229ffdd1e789fd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
