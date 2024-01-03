2da73d26c8fa79645121d77d66b8c5a45b5377b2
Santiago Rojas <santirojastr12@gmail.com>
2024-01-02 19:18:13 -0500

    Creación de Base de Datos

--- Backend/Models/StoreContext.cs
+++ Backend/Models/StoreContext.cs
@@ -4,7 +4,8 @@ namespace Backend.Models
 {
     public class StoreContext : DbContext
     {
-        //  Una forma de inicializar las migraciones es: Abrir la consola de paquetes NuGet, Add-Migration migrationName
+        //  Una forma de inicializar y agregar migraciones es: Abrir la consola de paquetes NuGet, Add-Migration migrationName
+        //  Update-Database -> Actualiza la base de datos a la ultima migracion
         public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
 
         public DbSet<Beer> Beers { get; set; }
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b030aa87653ab36b2834198649463ef0f59413c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
