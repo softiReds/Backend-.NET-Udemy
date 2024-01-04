fda38077db5cbc4aa67035eb57b95fc3539a8755
Santiago Rojas <santirojastr12@gmail.com>
2024-01-04 18:44:14 -0500

    Eliminar información (Delete)

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f4eb9b2b2664bffb6e362712830f42ae4530d2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10e2c1c38b4d4c002456033ccbbc03b50ff232b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
