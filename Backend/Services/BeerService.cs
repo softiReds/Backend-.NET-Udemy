c02d25e19d51a8653e18ccf0ee0ab8b6131fa78b
Santiago Rojas <santirojastr12@gmail.com>
2024-01-13 14:56:56 -0500

    Obtener información utilizando Repositorio (Read)

--- Backend/Services/BeerService.cs
+++ Backend/Services/BeerService.cs
@@ -1,5 +1,6 @@
 ﻿using Backend.DTOs;
 using Backend.Models;
+using Backend.Repository;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Services
@@ -7,17 +8,28 @@ namespace Backend.Services
     public class BeerService : ICommonService<BeerDto, BeerInsertDto, BeerUpdateDto>
     {
         private StoreContext _context;
+        private IRepository<Beer> _beerRepository;
 
-        public BeerService(StoreContext context) 
+        public BeerService(StoreContext context, IRepository<Beer> beerRepository) 
         {
             _context = context;
+            _beerRepository = beerRepository;
         }
 
-        public async Task<IEnumerable<BeerDto>> Get() => await _context.Beers.Select(e => new BeerDto { Id = e.BeerID, Name = e.Name, Alcohol = e.Alcohol, BrandID = e.BrandID }).ToListAsync();
+        public async Task<IEnumerable<BeerDto>> Get()
+        {
+            //  await _context.Beers.Select(e => new BeerDto { Id = e.BeerID, Name = e.Name, Alcohol = e.Alcohol, BrandID = e.BrandID }).ToListAsync();
+
+            var beers = await _beerRepository.Get();
+
+            return beers.Select(e => new BeerDto { Id = e.BeerID, Name = e.Name, Alcohol = e.Alcohol, BrandID = e.BrandID });
+        }
 
         public async Task<BeerDto> GetById(int id)
         {
-            var beer = await _context.Beers.FindAsync(id);
+            //  var beer = await _context.Beers.FindAsync(id);
+
+            var beer = await _beerRepository.GetById(id);
 
             if (beer != null)
             {
